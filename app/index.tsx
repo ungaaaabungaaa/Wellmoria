f6baa6d5a8241493cb9b142704f0cf40345e8710
syed <syed@kemuri.in>
2025-05-05 23:55:27 +0530

    added mulish font & background splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,31 @@
-import { View, Text, Button } from "react-native";
-import { useRouter } from "expo-router";
+import { Mulish_700Bold, useFonts } from "@expo-google-fonts/mulish";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  const router = useRouter();
+  const [fontsLoaded] = useFonts({
+    Mulish_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // or <AppLoading /> if you're using the older package
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ marginBottom: 20 }}>Welcome! Choose an option:</Text>
-      <Button title="Login" onPress={() => router.push("/login")} />
-      <View style={{ height: 10 }} />
-      <Button title="Register" onPress={() => router.push("/register")} />
-      <View style={{ height: 10 }} />
-      <Button title="Profile" onPress={() => router.push("/profile")} />
+    <View style={containerStyle}>
+      <Text style={textStyle}>Wellmoria</Text>
     </View>
   );
 }
+
+const containerStyle = {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#f7d2b7", // Light brown background
+};
+
+const textStyle = {
+  fontSize: 48,
+  fontFamily: "Mulish_700Bold",
+  color: "#c6a792", // Dark black text
+};
